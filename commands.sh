f27875520e51d7e2b487f7479068216e18efcbfc
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2023-09-30 19:41:49 +0400

    commit for az  commands

---
+++ commands.sh
@@ -0,0 +1,17 @@
+az group create --location westeurope --resource-group kapilkhubchandaniorg
+RG_ID=$(az group show --name $RG_NAME --query id --output tsv)
+
+#create service principal
+az ad sp create-for-rbac --name $SP_NAME --scopes $RG_ID --role Contributor --sdk-auth
+
+
+ SP_ID=$(az ad sp list --display-name $SP_NAME --query "[].appId" --output tsv)
+
+
+ #create azure container registry
+ az acr create --resource-group $RG_NAME --name $ACR_NAME --sku Basic  
+ 
+ ACR_ID=$(az acr show --name $ACR_NAME --query id --output tsv)
+
+ #assign permission to Service provider for ACR to push image
+ az role assignment create --assignee $SP_ID --scope $ACR_ID --role AcrPush
